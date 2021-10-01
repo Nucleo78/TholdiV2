7183653c1dda002f8d12298d214e863fd6e29510
Antoni <antoni.marques.lopes@gmail.com>
2021-10-01 15:14:15 +0200

    fix - connexion bdd

--- connexion.php
+++ connexion.php
@@ -7,7 +7,7 @@
     		<?php include 'menu.php';
              ?>
 
-<center>	
+<center></center>
 <body>
 	<img src=images/logo_tholdi.png>
 <h1>
@@ -25,35 +25,11 @@
 
 </ul>
 </form>
-<?php /*var_dump($_POST);echo '<br>';
-var_dump($_SESSION);
-$_SESSION['login'] = $_POST['login'];*/
-?>
 <!-- </table> -->
     </div>
 <form action="inscription.php">
   <button type="submit">Inscrivez-Vous</button>
 </form>
-<?php /*    
-$pdo = new PDO('mysql:host=localhost;dbname=tholdi', 'root','',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-$req = "select login,mdp,contact From utilisateur Where login like '".$_POST['login']."'";
-$pdoStatement = $pdo->query($req);
-$users = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
-//var_dump($users);
-$n = count($users);
-//echo $n;
-if ($n==1)
-{
-  //echo $users[0]['mdp'];
-  if ($users[0]['mdp'] == $_POST['mdp'])
-    {
-      //echo 'fonctionne';
-      //$_SESSION['contact'] = $users[3];
-      $_SESSION['contact'] = $users[0]['contact'];
-      //header('Location:index.php');
-    }
-}
-var_dump($_SESSION);*/
-?>
+
 </body>
 </html>
--- traitementConnexion.php
+++ traitementConnexion.php
@@ -1,8 +1,7 @@
-<?php session_start();
+<?php
+session_start();
 include 'gestion_fonctions.php';
 $login = $_POST["login"];
 $mdp = $_POST["mdp"];
-var_dump($_POST);
 connexion($login,$mdp);
 header("location:index.php");
-?>
